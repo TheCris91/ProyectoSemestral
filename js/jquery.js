61ea43a9a2847c91dffe6ebbe24ca281fd7021a5
Cristopher Arredondo <arredondocristopher43@gmail.com>
2023-05-08 00:27:25 -0400

    13

---
+++ js/jquery.js
@@ -0,0 +1,74 @@
+$(function(){
+    $("#Reg").validate({
+    rules:{
+        txtNombre:{
+            required: true,
+            maxlength:50,
+            minlength:3}
+        ,
+        txtRut:{
+            required: true,
+            maxlength:12,
+            minlength:11}
+        ,
+        txtGenero:{
+            required: true}
+        ,
+        txtDirecc:{
+            required: true}
+        ,
+        txtEmail:{
+            required: true}
+        ,
+        txtUser:{
+            required: true}
+        ,
+        txtPass:{
+            required: true}
+        ,
+        txtPassCon:{
+            required: true}
+        },
+        messages:{
+            txtNombre:{
+                required:"Campo obligatorio",
+                maxlength:"Excede la capacidad",
+                minlength:"Nombre no es valido"}
+            ,
+            txtRut:{
+                required:"Campo obligatorio",
+                maxlength:"Rut no valido",
+                minlength:"Rut no valido"}
+            ,
+            txtGenero:{
+                required:"Campo obligatorio"}
+            ,
+            txtDirecc:{
+                required:"Campo obligatorio"}
+            ,
+            txtEmail:{
+                required:"Campo obligatorio"}
+            ,
+            txtUser:{
+                required:"Campo obligatorio"}
+            ,
+            txtPass:{
+                required:"Campo obligatorio"}
+            ,
+            txtPassCon:{
+                required:"Campo obligatorio"}
+            }
+    })
+});
+function pass(){
+    let input = document.getElementById("txtPassword");
+    if (input.type == "password") {
+        input.type = "text";
+        document.getElementById("ocultar").style.display = "inline";
+        document.getElementById("mostrar").style.display = "none";
+    }else{
+        input.type = "password";
+        document.getElementById("ocultar").style.display = "none";
+        document.getElementById("mostrar").style.display = "inline";
+    }
+}
