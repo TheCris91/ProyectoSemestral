24b39130176f5b71f10026fc1a1c5221af9f1ef3
Cristopher Arredondo <arredondocristopher43@gmail.com>
2023-05-14 02:57:22 -0400

    19

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+{
+    id: 1,
+    nombre: "chamaedorea",
+    precio: 6000,
+    img:"img/chamaedorea.jpg",
+    cantidad: 1,
+},
+{
+    id: 2,
+    nombre: "Palo de agua",
+    precio: 8000,
+    img:"img/Palo_de_agua.jpg",
+    cantidad: 1,
+},
+{
+    id: 3,
+    nombre: "Lengua de suegra",
+    precio: 7000,
+    img:"img/Lengua-de-Suegra.jpg",
+    cantidad: 1,
+},
+{
+    id: 4,
+    nombre: "Suculenta",
+    precio: 6000,
+    img:"img/suculenta.jpg",
+    cantidad: 1,
+},
+];
